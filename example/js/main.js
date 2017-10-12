3cd75e995e445ceab2d1240151d9e2ca642bd797
Joshua Tai (HIE-TW) <joshua_tai@trend.com.tw>
2017-10-12 12:14:25 +0800

    Update

--- example/js/main.js
+++ example/js/main.js
@@ -1,13 +1,15 @@
 ;(function ($) {
 
-  var combobox = $('#combobox').combobox()
+  $('#combobox').combobox().add(
+  $('#combobox-2').combobox({
+    clearable: false
+  }))
     .on('change.combobox', function (e, value) {
       console.log(value);
     })
     .on('show.combobox', function (e, lists) {
       console.log(lists);
     });
-
 })(jQuery);
 
 
